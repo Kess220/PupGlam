da3f4678a8c7fe146ee9be9fd7e8a679c891ae72
Kess220 <prodvictorkaio@gmail.com>
2023-08-09 10:37:58 -0300

    curtidas

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -4,24 +4,85 @@ export const createPost = async (req, res) => {
   try {
     const { imagem_url, descricao, id_cachorro } = req.body;
 
-    // Obtém a data e hora atual
     const data_hora = new Date();
 
-    // Insere a nova postagem na tabela de postagens
     const query =
       "INSERT INTO postagens (imagem_url, descricao, data_hora, id_cachorro) VALUES ($1, $2, $3, $4) RETURNING *";
     const values = [imagem_url, descricao, data_hora, id_cachorro];
     const newPostagem = await db.query(query, values);
 
-    return res
-      .status(201)
-      .json({
-        message: "Postagem criada com sucesso",
-        postagem: newPostagem.rows[0],
-      });
+    return res.status(201).json({
+      message: "Postagem criada com sucesso",
+      postagem: newPostagem.rows[0],
+    });
   } catch (error) {
     return res
       .status(500)
       .json({ message: "Erro ao criar postagem", error: error.message });
+  }
+};
+
+export const getAllPosts = async (req, res) => {
+  try {
+    const posts = await db.query("SELECT * FROM postagens");
+    return res.status(200).json({ posts: posts.rows });
+  } catch (error) {
+    console.error("Erro ao obter postagens:", error);
+    return res
+      .status(500)
+      .json({ message: "Erro ao obter postagens", error: error.message });
+  }
+};
+
+export const curtirPostagem = async (req, res) => {
+  try {
+    const postId = req.params.id;
+    const userId = req.user.userId;
+
+    const existingPost = await db.query(
+      "SELECT * FROM postagens WHERE id = $1",
+      [postId]
+    );
+    if (existingPost.rows.length === 0) {
+      return res.status(404).json({ message: "Postagem não encontrada" });
+    }
+
+    const existingLike = await db.query(
+      "SELECT * FROM curtidas WHERE id_postagem = $1 AND id_usuario = $2",
+      [postId, userId]
+    );
+    if (existingLike.rows.length > 0) {
+      return res
+        .status(400)
+        .json({ message: "Usuário já curtiu esta postagem" });
+    }
+
+    const query =
+      "INSERT INTO curtidas (id_postagem, id_usuario, data_hora) VALUES ($1, $2, NOW())";
+    await db.query(query, [postId, userId]);
+
+    return res.status(201).json({ message: "Postagem curtida com sucesso" });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ message: "Erro ao curtir postagem", error: error.message });
+  }
+};
+
+export const getLikesCount = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    const result = await db.query(
+      "SELECT COUNT(*) AS likes_count FROM curtidas WHERE id_postagem = $1",
+      [postId]
+    );
+
+    const likesCount = result.rows[0].likes_count;
+
+    return res.status(200).json({ likesCount });
+  } catch (error) {
+    console.error("Erro ao obter número de curtidas:", error);
+    return res.status(500).json({ error: "Erro ao obter número de curtidas" });
   }
 };
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { createPost } from "../controllers/postController.js";
+import { getAllPosts } from "../controllers/postController.js";
 import { authenticate } from "../middlewares/authentication.js";
+import { curtirPostagem } from "../controllers/postController.js";
+import { getLikesCount } from "../controllers/postController.js";
 
 const router = express.Router();
 
 router.post("/", authenticate, createPost);
+router.get("/", authenticate, getAllPosts);
+router.post("/:id/curtir", authenticate, curtirPostagem);
+router.get("/:id/curtidas", authenticate, getLikesCount);
 
 export default router;
